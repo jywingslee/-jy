0b2745388bbf6653c7d4c9e1dcce3f3c8c9e9fe1
jywingslee <yeonjane@gmail.com>
2016-10-18 10:11:11 +0900

    added left_rotate function

--- 04.c
+++ 04.c
@@ -7,6 +7,17 @@ int head = 0;
 int tail = -1;
 int queue_size = 0;
 
+void left_rotate3(int arr[], int a, int b)
+{
+    int i;
+    int first = arr[b];
+    for(i=b; i<a; i++)
+    {
+        arr[i] = arr[i+1];
+    }
+    arr[b-1] = first;
+}
+
 void enqueue(int n)
 {
     if(queue_size == QUEUE_CAPACITY)
@@ -17,6 +28,9 @@ void enqueue(int n)
     tail++;
     queue_size++;
     queue[tail] = n;
+    
+    
+    printf("tail : %d, queue_size : %d \n", tail, queue_size);
 }
 
 int dequeue()
@@ -27,7 +41,18 @@ int dequeue()
         return 0;
     }
     queue_size--;
-    return queue[head++];
+    int r = queue[head];
+    //head++;
+    printf("head : %d, queue_size : %d \n", head, queue_size);
+    
+    //원소를 하나 꺼낼 때 마다 배열의 모든 원소를 한 칸씩 앞으로 움직이는 코드이다!
+    //이렇게 하면 배열의 앞쪽이 항상 채워져 있어 큐의 크기봅다 원소의 개수가 적다면 항상 큐에
+    //원소를 넣을 수 있다.
+    //하지만 큐에서 원소를 뺄 때마다 배열의 원소를 모두 한 칸씩 앞으로 이동 시키면
+    //시간이 많이 걸린다.
+    left_rotate3(queue, tail, head);
+    tail--;
+    return r;
 }
 
 //은행에서 대기 번호를 관리하는 프로그램을 작성해보자.
